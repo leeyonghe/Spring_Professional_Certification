47b6649e6639ed1b2f86a86ac3293ee4a3ee97d4
leeyh <leeyh@isung.com>
2020-01-05 16:28:03 +0900

    프로젝트등록

---
+++ bin/main/com/lee/kr/SpringCertificationServerApplication.kt
@@ -0,0 +1,11 @@
+package com.lee.kr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCertificationServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCertificationServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/lee/kr/SpringCertificationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lee.kr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCertificationServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
